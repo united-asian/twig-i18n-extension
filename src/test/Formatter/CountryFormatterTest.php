9b914089d9d0aff8bdc63b66d10374ccaa1784b8
uid=rabin <rabin@crossovernepal.com>
2015-06-18 10:29:31 +0545

    Added class CountryFormatterTest

---
+++ src/test/Formatter/CountryFormatterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace UAM\Twig\Extension\I18n\test\Formatter;
+
+use UAM\Twig\Extension\I18n\test\Formatter\AbstractFormatterTestCase;
+
+class CountryFormatterTest extends AbstractFormatterTestCase
+{
+
+    public function testFormatCountry() {
+
+        return ;
+    }
+
+
+    protected function getFormatter() {
+
+    }
+
+    protected function getIntlFormatter() {
+
+    }
+
+    protected function getLocale() {
+
+    }
+
+}

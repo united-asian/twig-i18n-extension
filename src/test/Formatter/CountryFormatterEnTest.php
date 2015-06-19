51f0e46f3f0a19373fe123017f55261d2387e6a8
uid=rabin <rabin@crossovernepal.com>
2015-06-19 12:21:25 +0545

    Added locale for en,fr and nolocale case individually

--- src/test/Formatter/CountryFormatterEnTest.php
+++ src/test/Formatter/CountryFormatterEnTest.php
@@ -2,48 +2,10 @@
 
 namespace UAM\Twig\Extension\I18n\test\Formatter;
 
-use UAM\Twig\Extension\I18n\Formatter\CountryFormatter;
-use UAM\Twig\Extension\I18n\test\Formatter\AbstractFormatterTestCase;
-use Symfony\Component\Intl\Intl;
-
-class CountryFormatterTest extends AbstractFormatterTestCase
+class CountryFormatterEnTest extends AbstractCountryFormatterTestCase
 {
-    /**
-     *@dataProvider CountryProvider
-     */
-    public function testFormatCountry($country, $expected) {
-
-         $formatted = $this->formatter->formatCountry($country, $this->locale);
-         $this->assertEquals($expected, $formatted);
-    }
-
-    public function countryProvider()
-    {
-        $countries = $this->intlFormatter->getCountryNames($this->locale);
-
-        foreach($countries as $id => $name) {
-            $country =  $id;
-
-            $expected = $name;
-
-            $data[] = array(strtolower($country), $expected);
-        }
-
-        return $data;
-    }
-
-    protected function getFormatter()
-    {
-        return new CountryFormatter();
-    }
-
-    protected function getIntlFormatter()
-    {
-        return Intl::getRegionBundle();
-    }
-
     protected function getLocale()
     {
-        return ('en');
+        return 'en';
     }
 }

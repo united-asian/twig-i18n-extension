81c8f52d455d5346377c23295b63745b6b08627f
Roshan <rcroshan63@gmail.com>
2016-09-05 13:41:12 +0545

    wrote test for basic test cases

--- test/PeriodExtensionTest.php
+++ test/PeriodExtensionTest.php
@@ -7,9 +7,10 @@ use UAM\Twig\Extension\I18n\PeriodExtension;
 class PeriodExtensionTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @dataProvider sameDateProvider
+     * @dataProvider dateProvider
+     *
      */
-    public function testSameDate($start_date, $end_date, $expected)
+    public function testDate($start_date, $end_date, $expected)
     {
         $this->getMockBuilder(PeriodExtension::class)
             ->disableOriginalConstructor()
@@ -45,12 +46,23 @@ class PeriodExtensionTest extends \PHPUnit_Framework_TestCase
         return $parameters['%from%'].' - '.$parameters['%to%'];
     }
 
-    public function sameDateProvider()
+    public function dateProvider()
     {
         return array(
+            //when start date is same as end date
             array('02-09-2016', '02-09-2016', '2 September 2016'),
             array('15-12-2015', '15-12-2015', '15 December 2015'),
-            array('10-10-2010', '10-10-2010', '10 October 2010'),
+
+            //when start date is less than end date
+            array('05-03-2016', '10-03-2016', '5 - 10 March 2016'),
+            array('12-05-2015', '24-05-2015', '12 - 24 May 2015'),
+            array('12-04-2016', '01-06-2016', '12 April - 1 June 2016'),
+            array('30-05-2015', '04-03-2016', '30 May 2015 - 4 March 2016'),
+
+            //when start date is greater than end date
+            array('05-02-2017', '10-03-2015', '5 February 2017 - 10 March 2015'),
+            array('20-04-2016', '10-04-2016', '20 - 10 April 2016'),
+            array('05-05-2016', '10-04-2016', '5 May - 10 April 2016'),
         );
     }
 }

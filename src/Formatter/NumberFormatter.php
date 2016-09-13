d396cb2e8074583d765d5f63bef3ec86d8362bcf
rijan <rijan@crossovernepal.com>
2016-09-13 14:39:23 +0545

    tested and implemented bytes filter for locale en

--- src/Formatter/NumberFormatter.php
+++ src/Formatter/NumberFormatter.php
@@ -2,21 +2,28 @@
 
 namespace UAM\Twig\Extension\I18n\Formatter;
 
-use Exception;
 use NumberFormatter as IntlNumberFormatter;
+use Symfony\Component\Translation\Loader\JsonFileLoader;
+use Symfony\Component\Translation\MessageSelector;
+use Symfony\Component\Translation\Translator;
 
 /**
  * Formats numeric values to localized number string.
  */
 class NumberFormatter extends AbstractFormatter
 {
+    const CATALOGUE = 'uam-18n';
+
+    protected $units;
+    private $translator;
+
     protected $bytes_units = array(
-        'b' => 0,
-        'k' => 1,
-        'm' => 2,
-        'g' => 3,
-        't' => 4,
-        'p' => 5,
+        'B' => 'bytes_unit.bytes',
+        'K' => 'bytes_unit.kilobytes',
+        'M' => 'bytes_unit.megabytes',
+        'G' => 'bytes_unit.gigabytes',
+        'T' => 'bytes_unit.terabytes',
+        'P' => 'bytes_unit.petabytes',
     );
 
     public function formatNumber($number, $locale = null)
@@ -84,48 +91,91 @@ class NumberFormatter extends AbstractFormatter
         return $formatter->format($number);
     }
 
-    public function formatBytes($bytes, $format = null, $locale)
+    public function formatBytes($bytes, $format, $locale = null)
     {
-        $format = strtolower($format);
+        $format = strtoupper($format);
 
-        if (!preg_match('/^(?:b|([kmgtp])b?)$/', $format, $matches)) {
+        $locale = $this->getLocale($locale);
+
+        $this->units = array_keys($this->bytes_units);
+
+        if (!preg_match('/^(?:B|([KMGTP])B?)$/', $format, $matches)) {
             $format = 'h';
         } else {
             if (isset($matches[1])) {
                 $format = $matches[1];
             }
         }
-        var_dump($format);
-
-
 
         if (!$bytes) {
-            return '0b';
+            return '0'.$this->trans('B', $locale);
         }
 
-        if ($format == 'h') {
-            $format = $this->getFormat($bytes);
-        }
-
-        $formatted_value = $this->formattedValue($bytes, $format);
+        $index = $this->getIndex($bytes, $format);
+        $formatted_value = $this->getFormattedValue($bytes, $index);
 
         if ($formatted_value < 1) {
-            $format = $this->getFormat($bytes);
-            $formatted_value = $this->formattedValue($bytes, $format);
+            $format = 'h';
+            $index = $this->getIndex($bytes, $format);
+            $formatted_value = $this->getFormattedValue($bytes, $index);
         }
 
-        return $formatted_value . $format;
+        return $formatted_value.$this->getFormat($index, $locale);
     }
 
-    protected function getFormat($bytes)
+    protected function getIndex($bytes, $format)
     {
-        $pow = floor(log($bytes) / log(1024));
+        if ($format == 'h') {
+            $index = floor((log($bytes, 1024)));
+        } else {
+            $index = array_search($format, $this->units);
+        }
 
-        return array_search($pow, $this->bytes_units);
+        return $index;
     }
 
-    protected function formattedValue($bytes, $format)
+    protected function getFormat($index, $locale)
     {
-        return floor($bytes / pow(1024, ($this->bytes_units[$format])));
+        $format = $this->units[$index];
+        $bytes_unit = $this->bytes_units[$format];
+
+        return $this->trans($bytes_unit, $locale);
+    }
+
+    protected function getFormattedValue($bytes, $index)
+    {
+        return floor($bytes / pow(1024, $index));
+    }
+
+    protected function trans($format, $locale)
+    {
+        $translator = $this->getTranslator();
+
+        return $translator->trans($format, array(), static::CATALOGUE, $locale);
+    }
+
+    protected function getTranslator()
+    {
+        if (!$this->translator) {
+            $this->translator = new Translator('en', new MessageSelector());
+
+            $this->translator->addLoader('json', new JsonFileLoader());
+
+            $this->translator->addResource(
+                'json',
+                dirname(__FILE__).'/uam-i18n.en.json',
+                'en',
+                static::CATALOGUE
+            );
+
+            $this->translator->addResource(
+                'json',
+                dirname(__FILE__).'/uam-i18n.fr.json',
+                'fr',
+                static::CATALOGUE
+            );
+        }
+
+        return $this->translator;
     }
 }
--- tests/Formatter/NumberFormatterTest.php
+++ tests/Formatter/NumberFormatterTest.php
@@ -69,30 +69,76 @@ class NumberFormatterTest extends AbstractFormatterTestCase
     }
 
     /**
-     * @dataProvider bytesProvider()
+     * @dataProvider bytesProviderEn()
      */
-    public function testByteFilter($number, $format, $expected, $locale = null)
+    public function testBytesFilterEn($number, $format, $expected)
     {
+        $locale = 'en';
+
         $this->assertEquals(
             $expected,
             $this->getFormatter()->formatBytes($number, $format, $locale)
         );
     }
 
-    public function bytesProvider()
+    public function bytesProviderEn()
     {
         return array(
-            array(1048576, 'b', '1048576b'),
-            array(9728, 'Kb', '9Kb'),
-            array(12897484, 'Mb', '12Mb'),
-            array(16106127360, 'Gb', '15Gb'),
-            array(7916483719987.2, 'Tb', '7Tb'),
-            array(9002199254340800, 'Pb', '7Pb'),
-            array(2048, 'Mb', '2Kb'),
-            array(2048, 'h', '2Kb'),
-            array(2056, 'b', '2056b'),
-            array(600, 'b', '600b'),
-            array('', 'Mb', '0b'),
+            //when format is B and b
+            array(1048576, 'B', '1048576B'),
+            array(1048576, 'b', '1048576B'),
+            array(600, 'B', '600B'),
+
+            //when format is k, kb, Kb, KB, kB
+            array(9728, 'k', '9KB'),
+            array(9728, 'kb', '9KB'),
+            array(9728, 'Kb', '9KB'),
+            array(9728, 'KB', '9KB'),
+            array(9728, 'kB', '9KB'),
+
+            //when format is m, M, mb, MB, mB
+            array(12897484, 'm', '12MB'),
+            array(12897484, 'M', '12MB'),
+            array(12897484, 'mb', '12MB'),
+            array(12897484, 'MB', '12MB'),
+            array(12897484, 'mb', '12MB'),
+
+            //when format is g, G, Gb, GB, gB
+            array(16106127360, 'g', '15GB'),
+            array(16106127360, 'G', '15GB'),
+            array(16106127360, 'Gb', '15GB'),
+            array(16106127360, 'GB', '15GB'),
+            array(16106127360, 'gB', '15GB'),
+
+            //when format is t, T, Tb, TB, tB
+            array(7916483719987.2, 't', '7TB'),
+            array(7916483719987.2, 'T', '7TB'),
+            array(7916483719987.2, 'Tb', '7TB'),
+            array(7916483719987.2, 'TB', '7TB'),
+            array(7916483719987.2, 'tB', '7TB'),
+
+            //when format is p, P, Pb, PB, pB
+            array(9002199254340800, 'p', '7PB'),
+            array(9002199254340800, 'P', '7PB'),
+            array(9002199254340800, 'Pb', '7PB'),
+            array(9002199254340800, 'PB', '7PB'),
+            array(9002199254340800, 'pB', '7PB'),
+
+            //when formatted bytes is less than 1 then automatically convert into nearest human readable
+            array(2048, 'M', '2KB'),
+
+            //when format is h then automatically convert into nearest human readable
+            array(2048, 'h', '2KB'),
+            array(2048, 'H', '2KB'),
+
+            //when bytes is null
+            array('', 'KB', '0B'),
+
+            //when format is other than specified format then
+            array(2048, 'A', '2KB'),
+
+            //when bytes and format is null
+            array('', '', '0B'),
         );
     }
 

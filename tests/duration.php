f45333fad6e7a6250aea8193769a97f021b9c71c
Olivier Pichon <op@united-asian.com>
2016-09-13 16:32:00 +0700

    added tests

--- tests/duration.php
+++ tests/duration.php
@@ -6,6 +6,10 @@ $data = array(
 	array('2010-01-03 00:00:00', '2010-01-06 00:00:00'),
 	array('2015-01-01', '2015-12-31'),
 	array('2016-01-01', '2016-12-31'),
+	array('2016-01-01 01:00:00', '2016-01-01 02:00:00'),
+	array('2010-11-01', '2010-12-01'),
+	array('2010-12-01', '2010-01-01'),
+	array('2010-02-01', '2010-03-01'),
 );
 
 foreach ($data as $raw) {
@@ -26,13 +30,14 @@ foreach ($data as $raw) {
 
 	if (!is_int($parsed['hour'])) {
 		$end .= ' 23:59:59';
+		$date_end = new DateTime($end);
+		$date_end->modify('+1 second');
+	} else {
+		$date_end = new DateTime($end);
 	}
-
-	$date_end = new DateTime($end);
-	$date_end->modify('+1 second');
 
 	$interval = date_diff($date_start, $date_end);
 
-	echo sprintf("%s > %s\n", $start, $end);
+	echo sprintf("%s > %s\n", $date_start->format('Y-m-d'), $date_end->format('Y-m-d'));
 	print_r($interval);
 }

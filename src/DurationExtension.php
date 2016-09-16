0fdbf94d14f668d93b5a56ec55cbffb2c089d2c2
deepakaryal <deepakaryal2050@gmail.com>
2016-09-16 13:39:46 +0545

    fixed coding standard

--- src/DurationExtension.php
+++ src/DurationExtension.php
@@ -123,8 +123,6 @@ class DurationExtension extends Twig_Extension
     {
         $raw = $this->getRawDateInterval($from, $to);
 
-
-
         // FIXME [OP 2016-09-15] Adjust the interval as appropriate
         $interval = $raw;
 
